574b8630e0fd7fca4564a23bc61e5dbc310403b2
Mykhail <mykhail.humennyi@kindgeek.com>
2021-05-30 19:14:48 +0300

    initial commit

--- src/handler.js
+++ src/handler.js
@@ -5,7 +5,7 @@ const puppeteer = require('puppeteer-extra');
 var CronJob = require('cron').CronJob;
 
 var job = new CronJob('* * * * * *', function() {
-    console.log('You will see this message every second');
+    message.channel.send('You will see this message every second');
 }, null, true, 'America/Los_Angeles');
 job.start();
 
