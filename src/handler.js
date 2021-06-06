725c4c93620203103afb5fff558cb87d314bc3de
Mykhail <mykhail.humennyi@kindgeek.com>
2021-06-06 10:37:04 +0300

    initial commit

--- src/handler.js
+++ src/handler.js
@@ -7,7 +7,11 @@ const stealthPlugin = require('puppeteer-extra-plugin-stealth');
 puppeteer.use(stealthPlugin());
 
 let userSteamIdMap = new Map();
-userSteamIdMap.set('','76561198055693338');
+userSteamIdMap.set('647147225604030495','76561198055693338');//rokidemon
+userSteamIdMap.set('597469698724200489','76561199013338796');//-1
+userSteamIdMap.set('697539216829186070','76561198112704674');//shprot
+userSteamIdMap.set('851000365590773850','76561198047919064');//savage
+userSteamIdMap.set('635180027822080030','76561198049236193');//orest
 
 
 const handler = {
@@ -61,51 +65,49 @@ const handler = {
 
     async s(message, args) {
         message.reply('Секунду, шукаю стату...');
-        message.reply(message.author.toString());
-        message.reply(message.author.id.toString());
-        message.reply(message.author.username.toString());
+        let userSteamId = userSteamIdMap.get(message.author.id);
 
 
-        // const path = 'https://csgostats.gg';
-        // const browser = await puppeteer.launch({args: ['--no-sandbox']});
-        // const page = await browser.newPage();
-        //
-        // await page.setUserAgent(userAgent.getRandom());
-        // await page.goto(path + '/player/76561198112704674#/matches');
-        // await page.waitForSelector('.table');
-        //
-        // const url = await page.$$eval('.table tbody tr:first-child.js-link', (trs) =>
-        //     trs.map((tr) => {
-        //         const attr = tr.getAttribute('onclick');
-        //         const url = attr.substr(attr.search('/match/'));
-        //         return url.substr(0, url.length - 2);
-        //     })
-        // );
+        const path = 'https://csgostats.gg';
+        const browser = await puppeteer.launch({args: ['--no-sandbox']});
+        const page = await browser.newPage();
 
-        // message.channel.send('', {
-        //     embed: {
-        //         color: 3447003,
-        //         title: 'Статистика',
-        //         url: path,
-        //         description: `Тут можна переглянути статистику усіх наших каток`,
-        //         thumbnail: {
-        //             url: 'https://cdn.discordapp.com/app-icons/769990595627319387/d284cace8d89541b7c5461fed41f161a.png?size=256',
-        //         },
-        //         fields: [
-        //             {
-        //                 name: 'Остання катка',
-        //                 value: `[:rainbow_flag:](${path + url})`,
-        //             },
-        //             {
-        //                 name: 'Усі катки за весь період',
-        //                 value: `[:pirate_flag:](${page.url()})`,
-        //             },
-        //         ],
-        //         timestamp: new Date(),
-        //     },
-        // });
+        await page.setUserAgent(userAgent.getRandom());
+        await page.goto(path + '/player/' + userSteamId + '#/matches');
+        await page.waitForSelector('.table');
 
-        // await browser.close();
+        const url = await page.$$eval('.table tbody tr:first-child.js-link', (trs) =>
+            trs.map((tr) => {
+                const attr = tr.getAttribute('onclick');
+                const url = attr.substr(attr.search('/match/'));
+                return url.substr(0, url.length - 2);
+            })
+        );
+
+        message.channel.send('', {
+            embed: {
+                color: 3447003,
+                title: 'Статистика',
+                url: path,
+                description: `Тут можна переглянути статистику усіх наших каток`,
+                thumbnail: {
+                    url: 'https://cdn.discordapp.com/app-icons/' + userSteamId + '/d284cace8d89541b7c5461fed41f161a.png?size=256',
+                },
+                fields: [
+                    {
+                        name: 'Остання катка',
+                        value: `[:rainbow_flag:](${path + url})`,
+                    },
+                    {
+                        name: 'Усі катки за весь період',
+                        value: `[:pirate_flag:](${page.url()})`,
+                    },
+                ],
+                timestamp: new Date(),
+            },
+        });
+
+        await browser.close();
     },
 };
 
